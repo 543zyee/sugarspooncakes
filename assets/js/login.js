a634186231889ccb2be16efa7fd810a2a351f9c1
ycczhan <v0963818504@gmail.com>
2025-06-10 23:16:19 +0800

    改表單阻擋功能

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,31 +1,32 @@
-//顯示密碼 密碼轉文字
-const checkbox=document.getElementById("showpassword");
-const passwordInput=document.getElementById("password");
-
-checkbox.addEventListener("change" , ()=>{
-    passwordInput.type=checkbox.checked? "text":"password" ;
-    }
-);
-
-//密碼識別功能
-const form = document.querySelector("form"); //找到網頁中的表單，把它存進 form 這個變數裡
-
-form.addEventListener("submit", function(event) {
-    const password = passwordInput.value; //取得使用者輸入的內容
-    
-    //8位英數密碼識別
-    const rule = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8}$/;
-    if (!rule.test(password)) {
-        alert("密碼格式錯誤：需為8位含英文與數字之密碼");
-        event.preventDefault(); // 阻止表單送出
-        return;
-    }
-});
 
 document.addEventListener("DOMContentLoaded", function () {
-    const form = document.querySelector("form");
-    form.addEventListener("submit", function (e) {
-        e.preventDefault(); // 防POST 
+    //顯示密碼 密碼轉文字
+    const checkbox=document.getElementById("showpassword");
+    const passwordInput=document.getElementById("password");
+
+    checkbox.addEventListener("change" , ()=>{
+        passwordInput.type=checkbox.checked? "text":"password" ;
+        }
+    );
+
+    //密碼識別功能
+    const form = document.querySelector("form"); //找到網頁中的表單，把它存進 form 這個變數裡
+
+    form.addEventListener("submit", function(event) {
+        const password = passwordInput.value; //取得使用者輸入的內容
+        
+        //8位英數密碼識別
+        const rule = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8}$/;
+        if (!rule.test(password)) {
+            alert("密碼格式錯誤：需為8位含英文與數字之密碼");
+            event.preventDefault(); // 阻止表單送出
+            return;
+        }
+        
+
+        event.preventDefault(); // 防post
         window.location.href = "member-center.html"; 
+
     });
+
 });
--- assets/js/register.js
+++ assets/js/register.js
@@ -1,53 +1,54 @@
-const checkbox=document.getElementById("showpassword1");
-const passwordInput=document.getElementById("password");
-
-checkbox.addEventListener("change" , ()=>{
-    passwordInput.type=checkbox.checked? "text":"password" ;
-    }
-);
-
-//顯示密碼 密碼轉文字
-const confirm_checkbox = document.getElementById("showpassword2");
-const confirm_passwordInput = document.getElementById("confirm-password");
-
-confirm_checkbox.addEventListener("change", () => {
-    confirm_passwordInput.type = confirm_checkbox.checked ? "text" : "password";
-    }
-);
-
-//密碼識別功能
-const form = document.querySelector("form"); //找到網頁中的表單，把它存進 form 這個變數裡
-const confirmPasswordInput = document.getElementById("confirm-password");
-
-form.addEventListener("submit", function(event) {
-    const password = passwordInput.value; //取得使用者輸入的內容
-    const confirmPassword = confirmPasswordInput.value;
-    
-    //8位英數密碼識別
-    const rule = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8}$/;
-    if (!rule.test(password)) {
-        alert("密碼格式錯誤：需為8位含英文與數字之密碼");
-        event.preventDefault(); // 阻止表單送出
-        return;
-    }
-    //確認密碼功能
-    if (password !== confirmPassword) {
-        alert("確認密碼與密碼不一致");
-        event.preventDefault(); // 阻止表單送出
-        return;
-    }
-    //同意規範才可送出
-    if (!this.acceptCharset.checked) {
-        alert("尚未同意使用規範");
-        event.preventDefault(); // 阻止表單送出
-        return;
-    }
-});
-
 document.addEventListener("DOMContentLoaded", function () {
-    const form = document.querySelector("form");
-    form.addEventListener("submit", function (e) {
-        e.preventDefault(); // 防POST 
+
+    const checkbox=document.getElementById("showpassword1");
+    const passwordInput=document.getElementById("password");
+
+    checkbox.addEventListener("change" , ()=>{
+        passwordInput.type=checkbox.checked? "text":"password" ;
+        }
+    );
+
+    //顯示密碼 密碼轉文字
+    const confirm_checkbox = document.getElementById("showpassword2");
+    const confirm_passwordInput = document.getElementById("confirm-password");
+
+    confirm_checkbox.addEventListener("change", () => {
+        confirm_passwordInput.type = confirm_checkbox.checked ? "text" : "password";
+        }
+    );
+
+    //密碼識別功能
+    const form = document.querySelector("form"); //找到網頁中的表單，把它存進 form 這個變數裡
+    const confirmPasswordInput = document.getElementById("confirm-password");
+    const acceptrule = document.getElementById("accept"); 
+
+    form.addEventListener("submit", function(event) {
+        const password = passwordInput.value; //取得使用者輸入的內容
+        const confirmPassword = confirmPasswordInput.value;
+        
+        //8位英數密碼識別
+        const rule = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8}$/;
+        if (!rule.test(password)) {
+            alert("密碼格式錯誤：需為8位含英文與數字之密碼");
+            event.preventDefault(); // 阻止表單送出
+            return;
+        }
+        //確認密碼功能
+        if (password !== confirmPassword) {
+            alert("確認密碼與密碼不一致");
+            event.preventDefault(); // 阻止表單送出
+            return;
+        }
+        //同意規範才可送出
+        if (!acceptrule.checked) {
+            alert("尚未同意使用規範");
+            event.preventDefault(); // 阻止表單送出
+            return;
+        }
+
+        event.preventDefault(); // 防post
         window.location.href = "member-center.html"; 
+
     });
+
 });
