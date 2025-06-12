5a58814bbf64cad97f67987e80b1f371d11a30de
ycczhan <v0963818504@gmail.com>
2025-06-12 09:50:21 +0800

    會員中心儲存

--- assets/js/member-center.js
+++ assets/js/member-center.js
@@ -39,3 +39,42 @@ document.addEventListener("DOMContentLoaded", function(){ //監聽觸發事件
         })
     })
 })
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    const checkbox=document.getElementById("showpassword1");
+    const passwordInput=document.getElementById("password");
+
+    checkbox.addEventListener("change" , ()=>{
+        passwordInput.type=checkbox.checked? "text":"password" ;
+        }
+    );
+
+    //顯示密碼 密碼轉文字
+    const confirm_checkbox = document.getElementById("showpassword2");
+    const confirm_passwordInput = document.getElementById("confirm-password");
+
+    confirm_checkbox.addEventListener("change", () => {
+        confirm_passwordInput.type = confirm_checkbox.checked ? "text" : "password";
+        }
+    );
+
+    //密碼識別功能
+    const form = document.querySelector("form"); //找到網頁中的表單，把它存進 form 這個變數裡
+    const acceptrule = document.getElementById("accept"); 
+
+    form.addEventListener("submit", function(event) {
+
+        //同意規範才可送出
+        if (!acceptrule.checked) {
+            alert("尚未同意使用規範");
+            event.preventDefault(); // 阻止表單送出
+            return;
+        }
+
+        event.preventDefault(); // 防post
+        window.location.href = "member-center.html"; 
+
+    });
+
+});
